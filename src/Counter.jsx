0b2fc2361605b3f438d7577e28a9bdebd476d3b7
kamaleldinmostafa87 <kamaleldin.mostafa87@gmail.com>
2024-05-23 16:02:20 +0300

    changed the commit message

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { increase, decrease } from './store'; // Import actions directly
+import { increase, decrease } from './counterSlice'; // Import actions directly
+import { login } from './auth';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter); // Access counter directly
+  const glopalState = useSelector((state) => state); // Access counter directly
 
   return (
     <>
-      <button onClick={() => dispatch(increase())}>
-        + increased count is {counter}
+   <button onClick={() => dispatch(login())}>
+        login
       </button>
+
+    {
+      glopalState.value.value ? <>
+      <button onClick={() => dispatch(increase())}>
+        + increased count is {glopalState.counter.counter }
+      </button>
+      
       <p>
         Edit <code>src/App.jsx</code> and save to test HMR
       </p>
@@ -18,6 +26,9 @@ const Counter = () => {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      </>:''
+    }
+      
     </>
   );
 };
