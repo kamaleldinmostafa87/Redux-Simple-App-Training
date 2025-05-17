706ecf6413e5801b9fc1cf232f0e71ec36ae3ba4
KamalEldin Mostafa <kmostafa.c@saip.gov.sa>
2025-05-18 02:34:31 +0300

    k

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -27,7 +27,7 @@ const Counter = () => {
           </p>
           <button onClick={() => dispatch(decrease())}> - Decrease</button>
           <p className="read-the-docs">
-            Click on the Vite and React logos to learn more yes
+            Click on the Vite and React logos to learn more yessss
           </p>
         </>
       ) : (
